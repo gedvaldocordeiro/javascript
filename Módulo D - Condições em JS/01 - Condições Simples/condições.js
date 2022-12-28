cd8cf87000fa58f2126dcd2c661418b82699ef8e
Gedvaldo Cordeiro Alves <gedvaldocordeiro@outlook.com>
2022-12-27 22:04:22 -0400

    Condições

---
+++ Módulo D - Condições em JS/01 - Condições Simples/condições.js
@@ -0,0 +1,11 @@
+// Condições Simples somente com o if, sem o else.
+
+var vel = 60
+
+console.log(`A velocidade do seu carro é de ${vel}Km/h.`)
+
+if(vel > 60) {
+    console.log('você ultrapassou a velocidade permitida. MULTADO!')
+}
+
+console.log('Dirija sempre com segurança.')
---
+++ Módulo D - Condições em JS/02 - Condições Compostas/compostas.js
@@ -0,0 +1,9 @@
+// Condições Compostas
+
+var pais = 'México'
+
+if (pais == 'Brasil') {
+    console.log(`Nascendo no ${pais} você é Brasileiro!`)
+} else {
+    console.log(`Nascendo no ${pais} você é estrangeiro!`)
+} 
